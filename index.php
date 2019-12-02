aac483f7bb24224e1ffa850af6b18b36fc0cd47c
Myogamevideo <alexandre.bougrat@hotmail.fr>
2019-12-02 09:11:27 +0100

    debut du projet

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>SoundPerception</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
