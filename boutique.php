aef8ee7c07dee59b6a218c0e1fcf2f63fb05f8f1
chchanson <chloe.chanson@etu.uca.fr>
2020-01-20 17:37:16 +0100

    commit avec la mise en place des article dans la boutique

--- boutique.php
+++ boutique.php
@@ -5,49 +5,188 @@
     <?php include('header-image.php') ?>
     <main>
         <div class="container">
-            <div class="slidershow">
-                <div class="slides ">
-                    <input type="radio" name="r" id="r1" checked>
-                    <input type="radio" name="r" id="r2">
-                    <input type="radio" name="r" id="r3">
-                    <input type="radio" name="r" id="r4">
-                    <input type="radio" name="r" id="r5">
-                    <div class="slide s1">
-                        <img src="public/image/background.jpg" alt="">
+            <div class="containerAll">
+                <div class="slidershow">
+                    <div class="slides ">
+                        <input type="radio" name="r" id="r1" checked>
+                        <input type="radio" name="r" id="r2">
+                        <input type="radio" name="r" id="r3">
+                        <input type="radio" name="r" id="r4">
+                        <input type="radio" name="r" id="r5">
+                        <div class="slide s1">
+                            <img src="public/image/background.jpg" alt="">
+                        </div>
+                        <div class="slide">
+                            <img src="public/image/background.jpg" alt="">
+                        </div>
+                        <div class="slide">
+                            <img src="public/image/background.jpg" alt="">
+                        </div>
+                        <div class="slide">
+                            <img src="public/image/background.jpg" alt="">
+                        </div>
+                        <div class="slide">
+                            <img src="public/image/background.jpg" alt="">
+                        </div>
                     </div>
-                    <div class="slide">
-                        <img src="public/image/background.jpg" alt="">
-                    </div>
-                    <div class="slide">
-                        <img src="public/image/background.jpg" alt="">
-                    </div>
-                    <div class="slide">
-                        <img src="public/image/background.jpg" alt="">
-                    </div>
-                    <div class="slide">
-                        <img src="public/image/background.jpg" alt="">
+                    <div class="navigation">
+                        <label for="r1" class="bar"></label>
+                        <label for="r2" class="bar"></label>
+                        <label for="r3" class="bar"></label>
+                        <label for="r4" class="bar"></label>
+                        <label for="r5" class="bar"></label>
                     </div>
                 </div>
-                <div class="navigation">
-                    <label for="r1" class="bar"></label>
-                    <label for="r2" class="bar"></label>
-                    <label for="r3" class="bar"></label>
-                    <label for="r4" class="bar"></label>
-                    <label for="r5" class="bar"></label>
+                <div class="boutique">
+                    <div class="filtre">
+                        <button class="btn success"><i class="fa fa-music"></i> CD</button>
+                        <button class="btn success"><i class="fa fa-music"></i> Vinyle</button>
+                        <button class="btn success"><i class="fa fa-music"></i> T-shirt</button>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-check-square"></i></span>Filtres :</li>
+                        </ul>
+                    </div>
+                    <div class="product">
+                        <div class="card">
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-product">
+                                <div class="card-image">
+                                    <img src="public/image/boutique1.jpg"/>
+                                </div>
+                                <div class="card-text">
+                                    <h3>Nom du produit</h3>
+                                </div>
+                                <div class="card-option">
+                                    <a href="#" class="prix">44€99</a>
+                                    <a href="#" class="aimer"><i class="fa fa-heart fa-2x" style="color:white;"></i></a>
+                                    <a href="#" class="caddie"><i class="fa fa-shopping-cart fa-2x" style="color:white;"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--
+                        <h3>Nom du produit</h3>
+                        <p>Un produit, dans son sens le plus général, désigne le résultat d'une activité humaine 
+                        sous la forme d'un bien ou d'un service. </p>
+                        <div class="divCarte">
+                        </div>
+                        <div class="produitColor">
+                            <span class="blue"></span>
+                            <span class="green"></span>
+                            <span class="yellow"></span>
+                            <span class="rose"></span>
+                        </div>
+                        <div class="produitInfo">
+                            <h3>90€99</h3>
+                            <a href="#" class="produitBoutton"></a>
+                            <a href="#" class="produitBoutton">Voir produit</a>
+                        </div>
+                        -->
                 </div>
-            </div>
-            <div class="boutique">
-                <div class="filtre">
-                    <button class="btn success"><i class="fa fa-music"></i> CD</button>
-                    <button class="btn success"><i class="fa fa-music"></i> Vinyle</button>
-                    <button class="btn success"><i class="fa fa-music"></i> T-shirt</button>
-                    <ul class="fa-ul">
-                        <li><span class="fa-li"><i class="fas fa-check-square"></i></span>Filtres :</li>
-                    </ul>
-                </div>
-                <div class="card">
-                    <img src="public/image/background.jpg" alt="" width="50">
-                </div>
+                
             </div>
         </div>
     </main>
