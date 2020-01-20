a9a83a4934ee914c5f0fcde07cea2b9c65ecd52c
Myogamevideo <alexandre.bougrat@hotmail.fr>
2020-01-20 17:35:12 +0100

    navigation-bar

--- navigation-bar.php
+++ navigation-bar.php
@@ -1,5 +1,5 @@
 <div class="icon-bar">
-    <a class="active" href="index.php"><i class="fa fa-home"> Accueil</i></a>
+    <a class="active" href="index.php"><i class="fa fa-home"></i> Accueil</a>
     <a href="boutique.php">Boutique</a>
     <a href="blog.php">Blog</a>
     <a href="#">Forum</a>
@@ -8,16 +8,17 @@
     <input type="text" placeholder="Search..">
     <button type="button"><i class="fa fa-search"></i></button>
     <div class="icon-bar-right">
-        <ul class="menu">
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-            <li><a href="connexion.php"><i class="fa fa-user-circle-o"></i></a>
-                <ul class="submenu">
-                    <li><a href="connexion.php">Connexion</a></li>
-                    <li><a href="inscription.php">Inscription</a></li>
-                    <li><a href="#">Mon compte</a></li>
-                    <li><a href="#">Deconnexion</a></li>
-                </ul>
-            </li>
-        </ul>     
+        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+        <div class="dropdown">
+            <button class="dropbtn">
+                <i class="fa fa-user-circle-o"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="connexion.php">Connexion</a>
+                <a href="inscription.php">Inscription</a>
+                <a href="#">Mon compte</a>
+                <a href="#">Deconnexion</a>
+            </div>
+        </div>
     </div>
 </div>
