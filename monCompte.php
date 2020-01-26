9cad649b70b22c8c9a1fb686baef19c8edfb1dc3
chchanson <chloe.chanson@etu.uca.fr>
2020-01-27 00:12:13 +0100

    Commit avec le debut de la mise en place de la page gestion de boutique

--- monCompte.php
+++ monCompte.php
@@ -7,17 +7,9 @@
             <div class="boxMonCompte">
                 <h2>Mon compte</h2>
                 <div class="divMonCompte">
-                    <div class="vertical-menu">
-                        <a href="#" class="active">Mon profil</a>
-                        <a href="#">Tableau de bord</a>
-                        <a href="#">Gestion des actualités</a>
-                        <a href="#">Gestion de la boutique</a>
-                        <a href="#">Liste des membres</a>
-                        <a href="#">Bannir un membre</a>
-                        <a href="#">Mes addresses</a>
-                        <a href="#">Mes abonnements</a>
-                        <a href="#">Se déconnecter</a>
-                    </div> 
+                    <div class="barreNavigation">
+                       <?php include('barreNavidationCompte.php')  ?>
+                    </div>
                     <div class="divInfoMembres">
                         <h3>Mes informations : </h3>
                         <div class="infoMembre">
