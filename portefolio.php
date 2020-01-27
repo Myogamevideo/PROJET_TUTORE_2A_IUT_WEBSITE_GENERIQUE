cf2ac9a9e27810915b694893fefb00cd2f6227a0
chchanson <chloe.chanson@etu.uca.fr>
2020-01-27 22:44:24 +0100

    commit avec les corresction du portefolio et du blog

--- portefolio.php
+++ portefolio.php
@@ -3,68 +3,71 @@
 <body>
     <?php include('header-image.php') ?>
     <main>     
-    <div class="container">
-            <div class="card">
-                <div class="imgBox">
-                    <img src="public/image/quiSommeNous.jpg"/>
+        <div class="container">
+            <div class="containerAll">
+                <h2>Portefolio :</h2>
+                <div class="card">
+                    <div class="imgBox">
+                        <img src="public/image/quiSommeNous.jpg"/>
+                    </div>
+                    <div class="contentBox">
+                        <div class="content">
+                            <h2>Qui sommes nous ?</h2>
+                            <p>bla</p>
+                            <a href="#">Lire plus ...</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="contentBox">
-                    <div class="content">
-                        <h2>Qui sommes nous ?</h2>
-                        <p>bla</p>
-                        <a href="#">Lire plus ...</a>
+                <div class="card">
+                    <div class="imgBox">
+                        <img src="public/image/quiSommeNous.jpg"/>
+                    </div>
+                    <div class="contentBox">
+                        <div class="content">
+                            <h2>Qui sommes nous ?</h2>
+                            <p>bla</p>
+                            <a href="#">Lire plus ...</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="imgBox">
+                        <img src="public/image/quiSommeNous.jpg"/>
+                    </div>
+                    <div class="contentBox">
+                        <div class="content">
+                            <h2>Qui sommes nous ?</h2>
+                            <p>bla</p>
+                            <a href="#">Lire plus ...</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="imgBox">
+                        <img src="public/image/quiSommeNous.jpg"/>
+                    </div>
+                    <div class="contentBox">
+                        <div class="content">
+                            <h2>Qui sommes nous ?</h2>
+                            <p>bla</p>
+                            <a href="#">Lire plus ...</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="imgBox">
+                        <img src="public/image/quiSommeNous.jpg"/>
+                    </div>
+                    <div class="contentBox">
+                        <div class="content">
+                            <h2>Qui sommes nous ?</h2>
+                            <p>bla</p>
+                            <a href="#">Lire plus ...</a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="card">
-                <div class="imgBox">
-                    <img src="public/image/quiSommeNous.jpg"/>
-                </div>
-                <div class="contentBox">
-                    <div class="content">
-                        <h2>Qui sommes nous ?</h2>
-                        <p>bla</p>
-                        <a href="#">Lire plus ...</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="imgBox">
-                    <img src="public/image/quiSommeNous.jpg"/>
-                </div>
-                <div class="contentBox">
-                    <div class="content">
-                        <h2>Qui sommes nous ?</h2>
-                        <p>bla</p>
-                        <a href="#">Lire plus ...</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="imgBox">
-                    <img src="public/image/quiSommeNous.jpg"/>
-                </div>
-                <div class="contentBox">
-                    <div class="content">
-                        <h2>Qui sommes nous ?</h2>
-                        <p>bla</p>
-                        <a href="#">Lire plus ...</a>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="imgBox">
-                    <img src="public/image/quiSommeNous.jpg"/>
-                </div>
-                <div class="contentBox">
-                    <div class="content">
-                        <h2>Qui sommes nous ?</h2>
-                        <p>bla</p>
-                        <a href="#">Lire plus ...</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div>    
     </main>
     <?php include('footer.php') ?>
 </body>
