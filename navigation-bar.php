61c32e73acfa3ff570e07a5b6f2a3e47d4ae2403
maduc2 <Maxime.DUC@etu.uca.fr>
2020-01-27 11:54:18 +0100

    mis a jour des fichiers liés à la navigation bar

--- navigation-bar.php
+++ navigation-bar.php
@@ -1,23 +1,21 @@
-<div class="icon-bar">
-    <a class="active" href="index.php"><i class="fa fa-home"></i> Accueil</a>
-    <a href="boutique.php">Boutique</a>
-    <a href="blog.php">Blog</a>
-    <a href="#">Forum</a>
-    <a href="portefolio.php">Portofolio</a>
-    <a href="contact.php">Contact</a>
-    <input type="text" placeholder="Search..">
-    <button type="button"><i class="fa fa-search"></i></button>
-    <div class="icon-bar-right">
-        <ul class="menu">
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-            <li><a href="connexion.php"><i class="fa fa-user-circle-o"></i></a>
-                <ul class="submenu">
-                    <li><a href="connexion.php">Connexion</a></li>
-                    <li><a href="inscription.php">Inscription</a></li>
-                    <li><a href="monCompte.php">Mon compte</a></li>
-                    <li><a href="deconnexion.php">Deconnexion</a></li>
-                </ul>
-            </li>
-        </ul>     
-    </div>
+<div class="menu">
+    <ul class="main-navigation">
+        <li><a class="active" href="index.php"><i class="fa fa-home"></i>Accueil</a></li>
+        <li><a href="boutique.php">Boutique</a></li>
+        <li><a href="blog.php">Blog</a></li>
+        <li><a href="#">Forum</a></li>
+        <li><a href="portefolio.php">Portofolio</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><input type="text" placeholder="Search.."></li>
+        <li><button type="button"><i class="fa fa-search"></i></button></li>
+        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+        <li><a href="connexion.php"><i class="fa fa-user-circle-o"></a>
+            <ul>
+                <li><a href="connexion.php">Connexion</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="monCompte.php">Mon compte</a></li>
+                <li><a href="deconnexion.php">Deconnexion</a></li>
+            </ul>
+        </li>
+    </ul>
 </div>
