05d60c3eac0af2665dcfaba1f89c36c33e46f87d
chchanson <chloe.chanson@etu.uca.fr>
2020-02-08 11:19:59 +0100

    Page tableau de bord

--- monCompteTableauDeBord.php
+++ monCompteTableauDeBord.php
@@ -3,6 +3,8 @@
 <body>
     <?php include('header-image.php') ?>
     <main>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript" src="public/js/diagrammeMonCompte.js"></script>
         <div class="container">
             <div class="boxMonCompte">
                 <div class="divMonCompte">
@@ -11,32 +13,77 @@
                     </div>
                     <div class="divTableauDeBord">
                         <h3>Tableau de bord : </h3>
-                        <div class="tanleauDeBord">
-                            <div>
+                        <div class="tableauDeBord">
+                            <div class="partieGauche">
                                 <div class="top10Article">
                                     <label>Top 10 des articles les plus vendus</label>
-                                    <ul class="borderAction listeProduitRupture">
+                                    <ul class="listeTop10Article">
                                         <li>
-                                            <label class="nomProduit 1">Produit1</label>
+                                            <label class="nomProduit">Produit1</label>
                                             <a href="#">Plus de détail»</a>
                                         </li>
                                         <li>
-                                            <label class="nomProduit 2">Produit1</label>
+                                            <label class="nomProduit">Produit2</label>
                                             <a href="#">Plus de détail»</a>
                                         </li>
                                         <li>
-                                            <label class="nomProduit 3">Produit1</label>
+                                            <label class="nomProduit">Produit3</label>
                                             <a href="#">Plus de détail»</a>
                                         </li>
                                         <li>
-                                            <label class="nomProduit 4">Produit1</label>
+                                            <label class="nomProduit">Produit4</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit5</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit7</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit7</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit8</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit9</label>
+                                            <a href="#">Plus de détail»</a>
+                                        </li>
+                                        <li>
+                                            <label class="nomProduit">Produit10</label>
                                             <a href="#">Plus de détail»</a>
                                         </li>
                                     </ul>
                                 </div>
-                            </div>
 
-                           
+                            </div>
+                            <div class="partieDroite">
+                                <div class="categorieArticlePlusVendu">
+                                <label>Catégorie d'article les plus vendu : </label>
+                                    <div id="donutchart" style=" height: 100%;"></div>
+                                </div>
+                                <div>
+                                    <label>Répartition des ventes en Europe : </label>
+                                    <div id="geochart-colors" style="width: 100%; height: 325px;"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="performanceDeLaCompagnie">
+                            <label>Performance de la compagnie : </label>
+                            <div>
+                                <div id="columnchart_material" style="width:100%; height: 400px;"></div>
+                            </div>
+                        </div>
+                        <div class="frequenceVisite">
+                            <label>Fréquence de visite du site : </label>
+                            <div>
+                                <div id="Aeraschart_div" style="width: 100%; height: 450px;"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
