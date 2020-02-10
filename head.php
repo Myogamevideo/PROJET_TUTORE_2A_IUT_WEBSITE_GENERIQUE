7cf49579674a7806213c4c7361dd9793d620c43a
Myogamevideo <alexandre.bougrat@hotmail.fr>
2020-02-10 09:18:21 +0100

    parametrage

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <?php
 try {
-  //  $bdd = new PDO('mysql:host=cfaifrnfzyg5.mysql.db;dbname=cfaifrnfzyg5;charset=utf8', 'cfaifrnfzyg5', 'Aiut2020');
-    $bdd =  new PDO("mysql:dbname=cfaifrnfzyg5;host=localhost;charset=utf8", 'root', '');
+    $bdd = new PDO('mysql:host=cfaifrnfzyg5.mysql.db;dbname=cfaifrnfzyg5;charset=utf8', 'cfaifrnfzyg5', 'Aiut2020');
+    //$bdd =  new PDO("mysql:dbname=cfaifrnfzyg5;host=localhost;charset=utf8", 'root', '');
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die('Erreur :' . $e->getMessage());
@@ -19,10 +19,14 @@ if (isset($_SESSION['status']) and $_SESSION['status'] == 'banni') {
 
 <head>
     <meta charset="utf-8">
-    <title>SoundPerception</title>
+    <?php
+    $req = $bdd->query('SELECT nomDuSite FROM parametres WHERE 1');
+    $donnees = $req->fetch()
+    ?>
+    <title><?php echo $donnees['nomDuSite']; ?></title>
     <link rel="stylesheet" href="public/css/style-body.css">
     <link rel="stylesheet" href="public/css/style-footer.css">
     <link rel="stylesheet" href="public/css/style-navigation-bar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script> 
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
 </head>
--- modifierSite.php
+++ modifierSite.php
@@ -1,5 +1,6 @@
 <?php include('head.php') ?>
 <link rel="stylesheet" href="public/css/style-modifierSite.css">
+
 <body>
     <?php include('header-image.php') ?>
     <main>
@@ -8,33 +9,52 @@
                 <h2>Mon compte</h2>
                 <div class="divMonCompte">
                     <div class="barreNavigation">
-                       <?php include('barreNavidationCompte.php')  ?>
+                        <?php include('barreNavidationCompte.php')  ?>
                     </div>
                     <div class="divModifierSite">
                         <h3>Tableau de bord : </h3>
                         <div class="tanleauDeBord">
+                            <?php
+                            $req = $bdd->query('SELECT nomDuSite FROM parametres WHERE 1');
+                            $donnees = $req->fetch()
+                            ?>
                             <div>
                                 <div>
-                                    <h4>Modification générale :</h4>
-                                    <div class="borderAction">
-                                        <label>Le nom de l'entreprise :</label>
-                                        <input id="modificationNomDuSite" type="text" name="modificationNomDuSite" require="">
-                                    </div>
-                                    <div class=button>
-                                        <button class="buttonAction">
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
-                                            Valider
-                                        </button>
-                                    </div>
+                                    <form method="POST" action="modifierSite.php">
+                                        <h4>Modification générale :</h4>
+                                        <div class="borderAction">
+                                            <label>Le nom de l'entreprise :</label>
+                                            <?php echo '<input id="modificationNomDuSite" name="modificationNomDuSite" type="text" placeholder="' . $donnees['nomDuSite'] . '" />'; ?>
+                                            <div class=button>
+                                                <input class="buttonAction" type="submit" value="Valider">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                                </input>
+                                            </div>
+                                    </form>
+                                </div>
+                                <div>
+                                    <form method="post" enctype="multipart/form-data">
+                                        <input type="file" name="photo">
+                                        <input type="submit">
+                                    </form>
+                                    <?php
+                                    if (isset($_FILES['photo']['tmp_name'])) {
+                                        $retour = copy($_FILES['photo']['tmp_name'], 'public/image/web/'.$_FILES['photo']['name']);
+                                        if ($retour) {
+                                            echo '<p>La photo a bien été envoyée.</p>';
+                                            echo '<img src="' . $_FILES['photo']['name'] . '">';
+                                        }
+                                    }
+                                    ?>
                                 </div>
                                 <div>
                                     <h4>Modification de l'espace vente</h4>
                                     <div class="borderAction">
                                         <label>Le nom de l'espace vente :</label>
-                                        <input id="espaceVente" type="text" name="espaceVente" require="">
+                                        <input id="espaceVente" type="text" name="espaceVente">
                                     </div>
                                     <div class=button>
                                         <button class="buttonAction">
@@ -111,14 +131,12 @@
     </main>
     <?php include('footer.php') ?>
 </body>
+
 </html>
 
-
-
-
-
-
-
-
-
-
+<?php
+if (isset($_POST['modificationNomDuSite']) && $_POST['modificationNomDuSite'] != NULL) {
+    $req = $bdd->prepare('UPDATE parametres SET nomDuSite=? where 1');
+    $req->execute(array($_POST['modificationNomDuSite']));
+}
+?>
