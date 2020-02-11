60cb96298202b0cd1388a1087776b49febefa210
chchanson <chloe.chanson@etu.uca.fr>
2020-02-11 22:06:28 +0100

    ajout de 2 nouvelles pages, 'qui nous sommes et nos projet'

--- nosProjets.php
+++ nosProjets.php
@@ -9,7 +9,54 @@
                 <h2>Nos projets :</h2>
                 <div class="contentAll">
                     <div class="divProjet">
-
+                        <div class="projet">
+                            <div class="circle">
+                                <img src="public/image/portefolio/projets/nosProjet.jpg">
+                                <div>
+                                    <h2>Titre du projet</h2>
+                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
+                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
+                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
+                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
+                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
+                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
+                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="projet">
+                            <div class="circle">
+                                <img src="public/image/portefolio/projets/nosProjet.jpg">
+                                <div>
+                                    <h2>Titre du projet</h2>
+                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
+                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
+                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
+                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
+                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
+                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
+                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="projet">
+                            <div class="circle">
+                                <img src="public/image/portefolio/projets/nosProjet.jpg">
+                                <div>
+                                    <h2>Titre du projet</h2>
+                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
+                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
+                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
+                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
+                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
+                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
+                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
