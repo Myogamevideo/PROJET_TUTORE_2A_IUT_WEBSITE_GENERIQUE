3b27bfc0d4ce8d0db667494132f80c4ddb4641fd
chchanson <chloe.chanson@etu.uca.fr>
2020-03-02 11:44:34 +0100

    commit avec mise en place possibilité modification sur portefolio/candidatureEmploie.php , portefolio/nosOffresDEmplois.php, portefolio/nosProjets.php, portefolio/portefolio.php et ajout des fonction associé

--- portefolio/nosOffresDEmplois.php
+++ portefolio/nosOffresDEmplois.php
@@ -9,8 +9,15 @@
                 <h2>Nos offres d'emplois :</h2>
                 <div class="candidature">
                     <div class="listeCandidature">
+                        <?php
+                            $reponse = $bdd->query('SELECT * FROM offreEmplois');
+                            while ($donnees = $reponse->fetch()) {
+                        ?>
                         <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
+                            <?php
+                                $chaine = $donnees['titreOffreEmplois'];
+                                echo '<h4>'. $chaine .'</h4>';
+                            ?>
                             <div class="card">
                                 <div class="offreEmploi">
                                     <div class="taille image">
@@ -23,124 +30,31 @@
                                     </div>
                                 </div>
                                 <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <?php
+                                        $chaine = $donnees['descriptionOffreEmplois'];
+                                        echo '<p>'. $chaine .'</p>';
+                                    ?>
                                     </div>
                                     <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
+                                        <?php
+                                            $chaine = $donnees['dateOffreEmplois'];
+                                            echo '<p>'. $chaine .'</p>';
+                                        ?>
+                                        <?php echo 
+                                            '
+                                            <a href="../portefolio/candidatureEmploie.php?offreEmplois=' . $donnees['idOffreEmplois'] . '>
+                                            </a>
+                                            <a href="../portefolio/candidatureEmploie.php?offreEmplois=' . $donnees['idOffreEmplois'] . '>
+                                                <i class="fa fa-angle-double-right fa-2x" style="color:black;"></i>
+                                            </a>'; 
+                                        ?>
+
                                     </div>
                             </div>
-                        </div>                
-                        <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
-                            <div class="card">
-                                <div class="offreEmploi">
-                                    <div class="taille image">
-                                        <img src="../public/image/offre_emploi/offreEmplois_consultant-informatique.jpg" alt="Consultant informatique">
-                                    </div>
-                                    <div class="taille option">
-                                        <p><i class="fa fa-briefcase" style="color:black;"></i><a href="#">Postulez ici !</a></p>                           
-                                        <p><i class="fa fa-star" style="color:black;"></i><a href="#">Intéressé(e) ?</a></p>                           
-                                        <p><i class="fa fa-share-alt" style="color:black;"></i><a href="#">Partager</a></p>                           
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
-                                    </div>
-                            </div>
-                        </div>     
-                        <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
-                            <div class="card">
-                                <div class="offreEmploi">
-                                    <div class="taille image">
-                                        <img src="../public/image/offre_emploi/offreEmplois_consultant-informatique.jpg" alt="Consultant informatique">
-                                    </div>
-                                    <div class="taille option">
-                                        <p><i class="fa fa-briefcase" style="color:black;"></i><a href="#">Postulez ici !</a></p>                           
-                                        <p><i class="fa fa-star" style="color:black;"></i><a href="#">Intéressé(e) ?</a></p>                           
-                                        <p><i class="fa fa-share-alt" style="color:black;"></i><a href="#">Partager</a></p>                           
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
-                                    </div>
-                            </div>
-                        </div>     
-                        <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
-                            <div class="card">
-                                <div class="offreEmploi">
-                                    <div class="taille image">
-                                        <img src="../public/image/offre_emploi/offreEmplois_consultant-informatique.jpg" alt="Consultant informatique">
-                                    </div>
-                                    <div class="taille option">
-                                        <p><i class="fa fa-briefcase" style="color:black;"></i><a href="#">Postulez ici !</a></p>                           
-                                        <p><i class="fa fa-star" style="color:black;"></i><a href="#">Intéressé(e) ?</a></p>                           
-                                        <p><i class="fa fa-share-alt" style="color:black;"></i><a href="#">Partager</a></p>                           
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
-                                    </div>
-                            </div>
-                        </div>     
-                        <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
-                            <div class="card">
-                                <div class="offreEmploi">
-                                    <div class="taille image">
-                                        <img src="../public/image/offre_emploi/offreEmplois_consultant-informatique.jpg" alt="Consultant informatique">
-                                    </div>
-                                    <div class="taille option">
-                                        <p><i class="fa fa-briefcase" style="color:black;"></i><a href="#">Postulez ici !</a></p>                           
-                                        <p><i class="fa fa-star" style="color:black;"></i><a href="#">Intéressé(e) ?</a></p>                           
-                                        <p><i class="fa fa-share-alt" style="color:black;"></i><a href="#">Partager</a></p>                           
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
-                                    </div>
-                            </div>
-                        </div>     
-                        <div class="offre">
-                            <h4>Chargé clientelle e-commerce (homme/femme)</h4>
-                            <div class="card">
-                                <div class="offreEmploi">
-                                    <div class="taille image">
-                                        <img src="../public/image/offre_emploi/offreEmplois_consultant-informatique.jpg" alt="Consultant informatique">
-                                    </div>
-                                    <div class="taille option">
-                                        <p><i class="fa fa-briefcase" style="color:black;"></i><a href="#">Postulez ici !</a></p>                           
-                                        <p><i class="fa fa-star" style="color:black;"></i><a href="#">Intéressé(e) ?</a></p>                           
-                                        <p><i class="fa fa-share-alt" style="color:black;"></i><a href="#">Partager</a></p>                           
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <h5>Last updated 3 mins ago</h5>
-                                        <a href="../portefolio/candidatureEmploie.php"><i class="fa fa-angle-double-right fa-2x" style="color:black;"></i></a>
-                                    </div>
-                            </div>
-                        </div>             
+                        </div> 
+                        <?php }
+                            $reponse->closeCursor(); 
+                        ?>
                     </div>
                 </div>
             </div>
--- portefolio/nosProjets.php
+++ portefolio/nosProjets.php
@@ -9,54 +9,28 @@
                 <h2>Nos projets :</h2>
                 <div class="contentAll">
                     <div class="divProjet">
+                        <?php
+                            $reponse = $bdd->query('SELECT * FROM projet');
+                            while ($donnees = $reponse->fetch()) {
+                        ?>
                         <div class="projet">
                             <div class="circle">
                                 <img src="../public/image/portefolio/projets/nosProjet.jpg">
                                 <div>
-                                    <h2>Titre du projet</h2>
-                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
-                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
-                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
-                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
-                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
-                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
-                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
-                                    </p>
+                                    <?php
+                                        $chaine = $donnees['titreProjet'];
+                                        echo '<p>'. $chaine .'</p>';
+                                    ?>
+                                    <?php
+                                        $chaine = $donnees['descriptionProjet'];
+                                        echo '<p>'. $chaine .'</p>';
+                                    ?>
                                 </div>
                             </div>
                         </div>
-                        <div class="projet">
-                            <div class="circle">
-                                <img src="../public/image/portefolio/projets/nosProjet.jpg">
-                                <div>
-                                    <h2>Titre du projet</h2>
-                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
-                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
-                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
-                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
-                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
-                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
-                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="projet">
-                            <div class="circle">
-                                <img src="../public/image/portefolio/projets/nosProjet.jpg">
-                                <div>
-                                    <h2>Titre du projet</h2>
-                                    <p>Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons 
-                                        et nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de 
-                                        sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance. 
-                                        Chez nous, même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et 
-                                        nous serons au quotidien, le partenaire de votre croissance.Chez nous, même les petits budgets sont traités avec autant de sérieux 
-                                        et d'implication que les gros. Nous vous accompagnerons et nous serons au quotidien, le partenaire de votre croissance.Chez nous, 
-                                        même les petits budgets sont traités avec autant de sérieux et d'implication que les gros. Nous vous accompagnerons et nous serons 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                        <?php }
+                            $reponse->closeCursor(); 
+                        ?>
                     </div>
                 </div>
             </div>
