257ce8d7f905cc8d89660d54e3724fc3009abe68
chchanson <chloe.chanson@etu.uca.fr>
2020-03-10 20:21:29 +0100

    commit avec desing sur la page liste des membres

--- admin/monCompteListeDesMembres.php
+++ admin/monCompteListeDesMembres.php
@@ -12,15 +12,22 @@
                         <?php include('../barreNavidationCompte.php')  ?>
                     </div>
                     <div class="divListeDesMembres">
+                        <h3>Liste des membres : </h3>
                         <div class="liste">
-                            <h3>Liste des membres : </h3>
                             <?php
                             $reponse = $bdd->query('SELECT * FROM membre ORDER BY date_inscription');
                             while ($donnees = $reponse->fetch()) {
                             ?>
                                 <div class="containerImage">
                                     <img src="../public/image/membre/<?php echo $donnees['pseudo']; ?>.jpg" alt="Avatar" class="image">
-                                    <div class="overlay simpleMembre">
+                                    <?php
+                                        if($donnees['statu']=='admin'){
+                                            echo '<div class="overlay admin">';
+                                        }
+                                        else{
+                                            echo '<div class="overlay simpleMembre">';
+                                        }
+                                    ?>
                                         <div class="text">
                                             <div class="avatar">
                                                 <img src="../public/image/membre/<?php echo $donnees['pseudo']; ?>.jpg" alt="Avatar">
@@ -30,11 +37,10 @@
                                                 <input type="hidden" name="pseudo" id="pseudo" value="<?php echo $donnees['pseudo']; ?>">
                                                 <p><?php echo $donnees['statu']; ?></p>
                                                 <input type="hidden" name="statu" id="statu" value="<?php echo $donnees['statu']; ?>">
-                                                <p><label>Nom : </label><?php echo $donnees['nom']; ?></p>
+                                                <p><?php echo $donnees['nom'].' '; ?><?php echo $donnees['prenom']; ?></p>
                                                 <input type="hidden" name="nom" id="nom" value="<?php echo $donnees['nom']; ?>">
-                                                <button type="submit">PrintToPDF</button>
-                                                <p><label>Prenom : </label><?php echo $donnees['prenom']; ?></p>
                                                 <input type="hidden" name="prenom" id="prenom" value="<?php echo $donnees['prenom']; ?>">
+                                                <button type="submit" class="btn">PrintToPDF</button>
                                                 <p><label>E-mail : </label><?php echo $donnees['email']; ?></p>
                                                 <input type="hidden" name="email" id="email" value="<?php echo $donnees['email']; ?>">
                                                 <p><label>Téléphone : </label><?php echo '+33' . $donnees['telephone']; ?></p>
@@ -47,8 +53,14 @@
                             $reponse->closeCursor();
                             ?>
                         </div>
-                        <form method="POST" action="../admin/allPrintToPDF.php">
-                            <button type="submit">AllPrintToPDF</button>
+                        <form class="allPrintToPDF" method="POST" action="../admin/allPrintToPDF.php">
+                            <button class="buttonAction" type="submit">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Imprimer tout les membres !
+                            </button>
                         </form>
                     </div>
                 </div>
